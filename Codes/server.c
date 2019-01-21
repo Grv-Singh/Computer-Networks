60686af7b5bc0cc7db28be7c8c7e94ef88c06295
Gaurav Singh <12825441+Grv-Singh@users.noreply.github.com>
2019-01-21 15:01:08 +0530

    Add files via upload

---
+++ Codes/server.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+  int udpSocket, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr, clientAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size, client_addr_size;
+  int i;
+
+  /*Create UDP socket*/
+  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*Bind socket with address struct*/
+  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverStorage;
+
+  while(1){
+    /* Try to receive any incoming UDP datagram. Address and port of 
+      requesting client will be stored on serverStorage variable */
+    nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+
+    /*Adding the 2 numbers seperated by a space*/
+    int sum = buffer[0]+buffer[2];
+
+    strcpy(buffer, "The Answer is:");
+
+    strcat(buffer, sum);
+
+    /*Convert message received to uppercase
+    for(i=0;i<nBytes-1;i++)
+      buffer[i] = toupper(buffer[i]);*/
+
+    /*Send uppercase message back to client, using serverStorage as the address*/
+    /*Send answer message back to client, using serverStorage as the address*/
+    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+  }
+
+  return 0;
+}
